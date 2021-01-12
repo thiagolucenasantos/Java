d401cd53772972974ab0eb67a584eec9fa3d65b3
Thiago <77245516+thiagolucenasantos@users.noreply.github.com>
2021-01-11 23:24:55 -0300

    segundo_java

---
+++ TIPOS_DE_DADOS/script.js
@@ -0,0 +1,25 @@
+// Boolean
+// Null
+// Undefined
+// Number
+// String
+
+//numeros são representados na cor azul
+//strings são representadas na cor preta
+
+
+var a = 15
+var b = "teste"
+
+// variavel null, quer dizer que está vazia literalmente
+var c = null;
+
+//variavel boleana, pode ser true ou false, para aplicações boleanas
+var d = true;
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+
