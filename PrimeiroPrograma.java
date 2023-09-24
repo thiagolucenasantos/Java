d857522d734a0a278ae032afe01941c185f5662a
Thiago <tlucenalive@gmail.com>
2023-09-24 09:20:28 -0300

    estrutura básica do main com saida com uma frase

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+
+	// ctrl + espaço já faz a busca auutomática e preenche
+	// ctrl + m - maximiza e minimiza a tela de digitação do código
+	// ctrl + shift + f11 - roda o programa
+	//ctrl +shift + f - formata o código para deixar em ordem
+	
+	/**
+	 * Java doc
+	 * @author Thiago
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+
+		System.out.println("Primeiro programa!!"); // syso + ctrl espaço já gerou a linha inteira
+	}
+}
