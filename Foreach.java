f7317b67795b2b9700d80502d26427090282b751
Thiago <tlucenalive@gmail.com>
2023-09-26 16:43:30 -0300

    array - exemplo Foreach

---
+++ Foreach.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class Foreach {
+
+	public static void main(String[] args) {
+		double[] notas = {9.9, 3.4, 6.2, 7.5};
+		
+		//exemplo foreach
+		    //tipo: double,  variável d, percorrendo o array com : notas
+		for (double d : notas) {
+			System.out.println(d);
+		}
+	}
+}
