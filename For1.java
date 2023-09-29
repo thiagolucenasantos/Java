72c91e3abfebf7a8ba3e0526903c94ea668cbdb4
Thiago <tlucenalive@gmail.com>
2023-09-29 10:34:01 -0300

    exemplos de for

---
+++ For1.java
@@ -0,0 +1,19 @@
+package controle;
+
+public class For1 {
+
+	public static void main(String[] args) {
+
+		// estrutura focada em uma determinada quantidade de repetições
+
+		for (int contador = 0; contador <= 10; contador++) {
+
+			System.out.printf("i = %d\n", contador);
+		}
+		
+		//laço infinito
+//		for(;;) {
+//			System.out.println("Fim!!");
+//		}
+	}
+}
